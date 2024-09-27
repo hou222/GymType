3e47605710efa7f65b0630d54440b3c3891ad001
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-09-27 12:13:36 +0100

    add mobile menu close button

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -65,6 +65,15 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           </div>
         </div>
       </div>
+      {!isAbobeMediaScreens && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
