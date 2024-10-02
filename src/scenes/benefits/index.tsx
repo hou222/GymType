34adfc40401ee059dfe467105dd8b97ffca52574
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-10-02 23:48:51 +0100

    add benefits section

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,14 @@
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import { SelectedPage } from "../../shared/types";
+
+type Props = { setSelectedPage: (value: SelectedPage) => void };
+
+const Benefits = ({ setSelctedPage }: Props) => {
+  return <div></div>;
+};
+
+export default Benefits;
