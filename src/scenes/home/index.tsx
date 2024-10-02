acd3ca7f20b5752cbc4626e045e6121274d17bbb
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-10-02 13:43:15 +0100

    add home file

---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,19 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { SelectedPage } from "../../shared/types";
+import ActionButton from "@/shared/ActionButton";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageText from "@/assets/HomePageText.png";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Home = ({ setSelectedPage }: Props) => {
+  return <div>Home</div>;
+};
+
+export default Home;
