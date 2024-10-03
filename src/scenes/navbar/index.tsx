54e6f362af8cdbc3663d56a129ed6af57c900089
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-10-03 11:28:09 +0100

    add heading of benefits section

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -20,7 +20,9 @@ const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
 
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div
+        className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+      >
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} gap-16 w-full`}>
             <img src={Logo} alt="logo" />
