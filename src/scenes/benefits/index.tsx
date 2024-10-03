14095f296a55c272309d0be9f9b7acfc0e73c109
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-10-03 17:59:39 +0100

    update benefits

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -6,10 +6,11 @@ import {
 import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
 import HText from "../../shared/HText";
-
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
 import { p } from "framer-motion/client";
 import { BenefitType } from "@/shared/types";
 import Benefit from "./Benefit";
+import ActionButton from "../../shared/ActionButton";
 
 const benefits: Array<BenefitType> = [
   {
@@ -82,6 +83,51 @@ const Benefits = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
+
+        <div>
+          <img
+            className="mx-auto"
+            src={BenefitsPageGraphic}
+            alt="benefits-page-graphic"
+          />
+          <div>
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
+                <div>
+                  <HText>
+                    MILLIONS OF HAPPY MEMBERS GETTING {""}
+                    <span className="text-primary-500">FIT</span>
+                  </HText>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <p className="my-5">
+                Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                Felis orci diam odio.
+              </p>
+              <p className="mb-5">
+                Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                enim mattis odio in risus nunc.
+              </p>
+            </div>
+
+            <div className="relative mt-16">
+              <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
