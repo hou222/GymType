80e58e4b8a4f4557e6035c220bc956d555337d76
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-10-05 17:47:58 +0100

    add contact us section

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,57 @@
+import { motion } from "framer-motion";
+import { SelectedPage } from "../../shared/types";
+import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import HText from "../../shared/HText";
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+  return (
+    <motion.section
+      id="contactus"
+      className="mx-auto w-5/6 pt-24 pb-32"
+      onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+    >
+      <motion.div
+        className="w-3/5"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: -50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+      >
+        <HText>
+          <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+        </HText>
+        <p className="my-5">
+          Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
+          sapien vel rhoncus. Placerat at in enim pellentesque. Nulla adipiscing
+          leo egestas nisi elit risus sit. Nunc cursus sagittis.
+        </p>
+      </motion.div>
+
+      <div className="mt-10 justify-between gap-8 md:flex">
+        <motion.div
+          className="mt-10 basis-3/5 md:mt-0"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <form target="_blank" onSubmit={onSbmit}></form>
+        </motion.div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default ContactUs;
