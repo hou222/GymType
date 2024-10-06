3ca90ba55d4b9d3b087402f81a32d006291dd688
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-10-07 00:01:31 +0100

    update footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses";
 import ContactUs from "@/scenes/contactUs";
-import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+import Footer from "@/scenes/footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
